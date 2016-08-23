b90e9b7feb65130b0eafce9cedddc9d37c54d4cb
Joel Jacobson <joel@trustly.com>
2016-08-23 13:56:46 +0200

    Continue to log all rows from pg_stat_activity for previously unseen waiting situations, but also always log the oldest row sorted by xact_start

--- FUNCTIONS/log_waiting.sql
+++ FUNCTIONS/log_waiting.sql
@@ -6,85 +6,54 @@ SET search_path TO public, pg_temp
 AS $FUNC$
 DECLARE
 _LogWaitingID integer;
-_NeverSeenBefore boolean;
+_SeenBefore boolean;
 _DumpPgStatActivity boolean;
 BEGIN
 
-_NeverSeenBefore := NOT EXISTS (SELECT 1 FROM cron.LogWaiting WHERE NumPIDs = _NumPIDs AND MaxSeconds = _MaxSeconds);
+_SeenBefore := EXISTS (SELECT 1 FROM cron.LogWaiting WHERE NumPIDs = _NumPIDs AND MaxSeconds = _MaxSeconds);
 INSERT INTO cron.LogWaiting (NumPIDs, MaxSeconds) VALUES (_NumPIDs, _MaxSeconds) RETURNING LogWaitingID INTO STRICT _LogWaitingID;
-IF _NeverSeenBefore THEN
-    IF (SELECT setting::integer FROM pg_catalog.pg_settings WHERE pg_catalog.pg_settings.name = 'server_version_num') < 90600 THEN
-        INSERT INTO cron.LogWaitingPgStatActivity (
-            LogWaitingID,
-            datid,
-            datname,
-            procpid,
-            usesysid,
-            usename,
-            application_name,
-            client_addr,
-            client_hostname,
-            client_port,
-            backend_start,
-            xact_start,
-            query_start,
-            waiting,
-            current_query
-        )
-        SELECT
-            _LogWaitingID,
-            datid,
-            datname,
-            procpid,
-            usesysid,
-            usename,
-            application_name,
-            client_addr,
-            client_hostname,
-            client_port,
-            backend_start,
-            xact_start,
-            query_start,
-            waiting,
-            current_query
-        FROM pg_stat_activity;
-    ELSE
-        INSERT INTO cron.LogWaitingPgStatActivity (
-            LogWaitingID,
-            datid,
-            datname,
-            procpid,
-            usesysid,
-            usename,
-            application_name,
-            client_addr,
-            client_hostname,
-            client_port,
-            backend_start,
-            xact_start,
-            query_start,
-            waiting,
-            current_query
-        )
-        SELECT
-            _LogWaitingID,
-            datid,
-            datname,
-            pid,
-            usesysid,
-            usename,
-            application_name,
-            client_addr,
-            client_hostname,
-            client_port,
-            backend_start,
-            xact_start,
-            query_start,
-            waiting,
-            current_query
-        FROM pg_stat_activity;
-    END IF;
-END IF;
+
+EXECUTE format($SQL$
+INSERT INTO cron.LogWaitingPgStatActivity (
+    LogWaitingID,
+    datid,
+    datname,
+    procpid,
+    usesysid,
+    usename,
+    application_name,
+    client_addr,
+    client_hostname,
+    client_port,
+    backend_start,
+    xact_start,
+    query_start,
+    waiting,
+    current_query
+)
+SELECT
+    %s,
+    datid,
+    datname,
+    %I,
+    usesysid,
+    usename,
+    application_name,
+    client_addr,
+    client_hostname,
+    client_port,
+    backend_start,
+    xact_start,
+    query_start,
+    waiting,
+    current_query
+FROM pg_stat_activity
+%s
+$SQL$,
+    _LogWaitingID,
+    CASE WHEN (SELECT setting::integer FROM pg_catalog.pg_settings WHERE pg_catalog.pg_settings.name = 'server_version_num') < 90600 THEN 'procpid' ELSE 'pid' END,
+    CASE WHEN _SeenBefore THEN 'ORDER BY xact_start LIMIT 1' END
+);
 
 RETURN TRUE;
 END;
