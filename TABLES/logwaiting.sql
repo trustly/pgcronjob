8a68c837bcd9f1b5fb7bc58c6c4dd4baeadc09e5
Joel Jacobson <joel@trustly.com>
2016-12-05 10:37:14 +0100

    Remove KillIfWaiting feature. This functionality will instead by provided by PgTerminator.

--- TABLES/logwaiting.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE cron.LogWaiting (
-LogWaitingID        serial  NOT NULL,
-NumPIDs             integer NOT NULL,
-MaxSeconds          integer NOT NULL,
-Datestamp           timestamptz NOT NULL DEFAULT now(),
-PRIMARY KEY (LogWaitingID)
-);
-
-ALTER TABLE cron.LogWaiting OWNER TO pgcronjob;
