f143d391da225e9caf1d0b0fc873ec012ef6e0e2
Joel Jacobson <joel@trustly.com>
2016-12-08 14:22:37 +0700

    Move part of install.sql to new file cron.sql which will assume a clean database
    Continue to use install.sql if you want to re-install and drop the schema first

---
+++ cron.sql
@@ -0,0 +1,30 @@
+CREATE SCHEMA cron;
+
+ALTER SCHEMA cron OWNER TO pgcronjob;
+REVOKE ALL ON SCHEMA cron FROM PUBLIC;
+GRANT ALL ON SCHEMA cron TO pgcronjob;
+
+\ir FUNCTIONS/pg_stat_activity_portable.sql
+\ir TABLES/connectionpools.sql
+\ir TABLES/jobs.sql
+\ir TABLES/processes.sql
+\ir TABLES/log.sql
+\ir TABLES/errorlog.sql
+\ir FUNCTIONS/log_error.sql
+\ir FUNCTIONS/log_table_access.sql
+\ir FUNCTIONS/is_valid_function.sql
+\ir FUNCTIONS/register.sql
+\ir FUNCTIONS/disable.sql
+\ir FUNCTIONS/disable_process.sql
+\ir FUNCTIONS/enable.sql
+\ir FUNCTIONS/enable_process.sql
+\ir FUNCTIONS/run.sql
+\ir FUNCTIONS/dispatch.sql
+\ir FUNCTIONS/terminate_all_backends.sql
+\ir FUNCTIONS/new_connection_pool.sql
+\ir FUNCTIONS/reset_runattime.sql
+\ir FUNCTIONS/schedule.sql
+\ir FUNCTIONS/generate_register_commands.sql
+\ir VIEWS/vjobs.sql
+\ir VIEWS/vprocesses.sql
+\ir VIEWS/vlog.sql
--- install.sql
+++ install.sql
@@ -10,34 +10,5 @@ BEGIN;
 DROP SCHEMA cron CASCADE;
 COMMIT;
 BEGIN;
-CREATE SCHEMA cron;
-\ir FUNCTIONS/pg_stat_activity_portable.sql
-\ir TABLES/connectionpools.sql
-\ir TABLES/jobs.sql
-\ir TABLES/processes.sql
-\ir TABLES/log.sql
-\ir TABLES/errorlog.sql
-\ir FUNCTIONS/log_error.sql
-\ir FUNCTIONS/log_table_access.sql
-\ir FUNCTIONS/is_valid_function.sql
-\ir FUNCTIONS/register.sql
-\ir FUNCTIONS/disable.sql
-\ir FUNCTIONS/disable_process.sql
-\ir FUNCTIONS/enable.sql
-\ir FUNCTIONS/enable_process.sql
-\ir FUNCTIONS/run.sql
-\ir FUNCTIONS/dispatch.sql
-\ir FUNCTIONS/terminate_all_backends.sql
-\ir FUNCTIONS/new_connection_pool.sql
-\ir FUNCTIONS/reset_runattime.sql
-\ir FUNCTIONS/schedule.sql
-\ir FUNCTIONS/generate_register_commands.sql
-\ir VIEWS/vjobs.sql
-\ir VIEWS/vprocesses.sql
-\ir VIEWS/vlog.sql
-GRANT USAGE ON SCHEMA cron TO pgcronjob;
-GRANT SELECT,UPDATE ON TABLE cron.Jobs TO pgcronjob;
-GRANT INSERT ON TABLE cron.Log TO pgcronjob;
-GRANT USAGE ON SEQUENCE cron.log_logid_seq TO pgcronjob;
-
+\ir cron.sql
 COMMIT;
